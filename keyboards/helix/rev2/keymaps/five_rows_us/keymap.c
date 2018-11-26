8410510927119d5b0fc38a34eaf68cb56278e004
PiroHiroPiro <hirohiro11071107@gmail.com>
2018-11-26 22:36:39 +0900

    modify helix:five_rows_us

--- keyboards/helix/rev2/keymaps/five_rows_us/keymap.c
+++ keyboards/helix/rev2/keymaps/five_rows_us/keymap.c
@@ -62,23 +62,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Qwerty
    * ,-----------------------------------------.             ,-----------------------------------------.
-   * | Esc  |   1  |   2  |   3  |   4  |   5  |             |   6  |   7  |   8  |   9  |   0  |  |   |
+   * |  `   |   1  |   2  |   3  |   4  |   5  |             |   6  |   7  |   8  |   9  |   0  |  =   |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Tab  |   Q  |   W  |   E  |   R  |   T  |             |   Y  |   U  |   I  |   O  |   P  | Del  |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |  `   |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |Enter |
+   * |Adjust|   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |  ;   |Enter |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * | Shift|   Z  |   X  |   C  |   V  |   B  |  '   |  -   |   N  |   M  |   ,  |   .  |   /  |  =   |
+   * | Shift|   Z  |   X  |   C  |   V  |   B  |  ,   |  .   |   N  |   M  |  /   |  -   |  Up  |  |   |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |Adjust| Ctrl | Alt  | CMD  |Lower |Space |   [  |   ]  |Space |Raise | Left | Down |  Up  |Right |
+   * | Esc  | Ctrl | Alt  | CMD  |Lower |Space |   [  |   ]  |Space |Raise |  '   | Left | Down |Right |
    * `-------------------------------------------------------------------------------------------------'
    */
    [_QWERTY] = LAYOUT( \
        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL, \
        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-       KC_GRV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
-       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_QUOT, KC_MINS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_PIPE , \
-       ADJUST,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_LBRC, KC_RBRC, KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+       ADJUST,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
+       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_COMM, KC_DOT,  KC_N,    KC_M,    KC_SLSH, KC_MINS, KC_UP,   KC_PIPE , \
+       KC_ESC,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_LBRC, KC_RBRC, KC_SPC,  RAISE,   KC_QUOT, KC_LEFT, KC_DOWN, KC_RGHT \
        ),
 
   /* Colemak
